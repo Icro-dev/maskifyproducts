54292ba49b56bdcb00004b239e47903456cfd01e
Icro-dev <svenmuste@gmail.com>
2021-01-29 06:51:24 +0100

    Initial commit1

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
+
+
+// Route::get('/about', function () {
+//     return view('pages.about') ;
+// });
+
+// Route::get('/users/{id}/{name}', function ($id, $name) {
+//     return ' This is user ' .$name. ' with an id of ' .$id;
+// });
+
+
